3f8f1b0e302a80f02b013578fa2c07c50ee84036
Pablo Arzu <jptomas38@gmail.com>
2023-03-28 09:44:15 -0600

    webBank.cy.js: transfer found test created

--- cypress/e2e/webBank.cy.js
+++ cypress/e2e/webBank.cy.js
@@ -5,6 +5,7 @@ describe('Web Bank Security Test', () => {
     const url = 'http://zero.webappsecurity.com/'
     const username = 'username'
     const password = 'password'
+    const amount = 500
 
     it('smoke test', () => {
         cy.visit(url)
@@ -17,6 +18,14 @@ describe('Web Bank Security Test', () => {
         webBank.inputUsername(username)
         webBank.inputPassword(password)
         webBank.clickSignBtn()
+
+        webBank.transferFoundsClick() 
+        webBank.selectFromAccount(1)
+        webBank.selectToAccount(3)
+        webBank.totalAmount(amount)
+        webBank.sendDescription('description')
+        webBank.continueBtnClick()
+        webBank.submitBtnClick()
     })
 
 })
