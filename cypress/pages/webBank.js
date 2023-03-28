297b7951fc26bf849d63670b8834b881e679512c
Pablo Arzu <jptomas38@gmail.com>
2023-03-28 09:11:22 -0600

    webBank.js: elements Created

--- cypress/pages/webBank.js
+++ cypress/pages/webBank.js
@@ -1,4 +1,12 @@
 
 class webBank {
 
+    elements = {
+        brandText: () => cy.get('.brand'),
+        signinBtn: () => cy.get('#signin_button'),
+        loginInput: () => cy.get('#user_login'),
+        passwordInput: () => cy.get('#user_password'),
+        signBtn: () => cy.get('.btn')
+    }
+
 }
