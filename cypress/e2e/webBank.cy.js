1461cd8b7519589c3a89fdc9aeb7ab93c4b7eefb
Pablo Arzu <jptomas38@gmail.com>
2023-03-28 09:36:54 -0600

    webBank.cy.js: login test created

--- cypress/e2e/webBank.cy.js
+++ cypress/e2e/webBank.cy.js
@@ -1,10 +1,22 @@
+import webBank from "../pages/webBank"
 
 describe('Web Bank Security Test', () => {
    
     const url = 'http://zero.webappsecurity.com/'
+    const username = 'username'
+    const password = 'password'
 
     it('smoke test', () => {
         cy.visit(url)
+    })
+
+    it('Login test', () => {
+        cy.visit(url)
+        
+        webBank.clickSigninBtn() 
+        webBank.inputUsername(username)
+        webBank.inputPassword(password)
+        webBank.clickSignBtn()
     })
 
 })
