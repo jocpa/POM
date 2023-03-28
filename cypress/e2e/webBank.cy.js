ecef6b6c20650f48d8f45e8e08ab955372cd49be
Pablo Arzu <jptomas38@gmail.com>
2023-03-28 10:38:11 -0600

    webBank.cy.js: location verify

--- cypress/e2e/webBank.cy.js
+++ cypress/e2e/webBank.cy.js
@@ -1,3 +1,5 @@
+///<reference types="cypress" />
+
 import webBank from "../pages/webBank"
 
 describe('Web Bank Security Test', () => {
@@ -7,25 +9,38 @@ describe('Web Bank Security Test', () => {
     const password = 'password'
     const amount = 500
 
-    it('smoke test', () => {
+    beforeEach(() => {
         cy.visit(url)
     })
 
+    it('smoke test', () => {
+        cy.title().should('eq', 'Zero - Personal Banking - Loans - Credit Cards')
+        cy.url().should('eq', url)
+        cy.location('protocol').should('contains', 'http')
+    })
+
     it('Login test', () => {
-        cy.visit(url)
         
         webBank.clickSigninBtn() 
+        cy.location('pathname').should('eq', '/login.html')
+
         webBank.inputUsername(username)
         webBank.inputPassword(password)
         webBank.clickSignBtn()
 
+
         webBank.transferFoundsClick() 
+        cy.location('pathname').should('eq', '/bank/transfer-funds.html')
+
         webBank.selectFromAccount(1)
         webBank.selectToAccount(3)
         webBank.totalAmount(amount)
         webBank.sendDescription('description')
         webBank.continueBtnClick()
+        cy.location('pathname').should('eq', '/bank/transfer-funds-verify.html')
+
         webBank.submitBtnClick()
+        cy.location('pathname').should('eq', '/bank/transfer-funds-confirm.html')
 
         webBank.elements.succesAlert().should('have.class', 'alert-success')
     })
