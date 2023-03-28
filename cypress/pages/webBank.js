7a7e28360c1e0d7e4e0a4b2661e4be5e02a8c0e9
Pablo Arzu <jptomas38@gmail.com>
2023-03-28 09:23:22 -0600

    webBank.js: elements updated

--- cypress/pages/webBank.js
+++ cypress/pages/webBank.js
@@ -4,9 +4,20 @@ class webBank {
     elements = {
         brandText: () => cy.get('.brand'),
         signinBtn: () => cy.get('#signin_button'),
-        loginInput: () => cy.get('#user_login'),
+        loginUsername: () => cy.get('#user_login'),
         passwordInput: () => cy.get('#user_password'),
-        signBtn: () => cy.get('.btn')
+        signBtn: () => cy.get('.btn'),
+        transferFounds: () => cy.get('#transfer_funds_tab > a'),
+        fromAccount: () => cy.get('#transfer_funds_tab > a'),
+        toAccount: () => cy.get('#tf_toAccountId'),
+        amount: () => cy.get('#tf_amount'),
+        description: () => cy.get('#tf_description'),
+        continueBtn: () => cy.get('#btn_submit'),
+        submitBtn: () => cy.get('#btn_submit'),
+        succesAlert: () => cy.get('.alert')
     }
+  
+    
+    
 
 }
