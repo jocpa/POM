e343a554ebba75743e3718ff7298ad5156f70739
Pablo Arzu <jptomas38@gmail.com>
2023-03-29 08:21:03 -0600

    amazonTest.js: elements Created

--- cypress/pages/amazonTest.js
+++ cypress/pages/amazonTest.js
@@ -1,7 +1,12 @@
 class amazonTest {
 
     elements = {
-        
+        navLogo: () => cy.get('#nav-logo-sprites'),
+        searchBar: () => cy.get('#twotabsearchtextbox'),
+        nextPage: () => cy.get('.s-pagination-next'),
+        thirdElement: () => cy.xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[4]/div/div/div'),
+        addToCart: () => cy.xpath('/html/body/div[1]/div[2]/div[9]/div[6]/div[1]/div[4]/div/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div/form/div/div/div[13]/div[1]/span/span/span/input'),
+        addedAlert: () => cy.get('#attachDisplayAddBaseAlert > .a-box-inner > .a-alert-heading')
     }
 
 }
