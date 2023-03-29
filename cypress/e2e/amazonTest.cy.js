6f52b79ccf9b2bb1bff938c442e280f57d845630
Pablo Arzu <jptomas38@gmail.com>
2023-03-29 08:58:20 -0600

    amazonTest updated

--- cypress/e2e/amazonTest.cy.js
+++ cypress/e2e/amazonTest.cy.js
@@ -1,8 +1,29 @@
-describe('Amazon Test', () => {
-    const url = 'www.amazon.com';
+import amazonTest from "../pages/amazonTest";
 
-    it('Visita amazon.com', () => {
-        cy.visit(url)
+///<reference types="cypress" />
+
+describe('Amazon Test', () => {
+    const url = 'https://www.amazon.com/';
+
+    beforeEach(() => {
+       cy.visit(url)
+    });
+
+    it('Smoke Test', () => {
+        cy.title().should('eq','Amazon.com. Spend less. Smile more.')
+        cy.url().should('eq', url)
+        cy.location('protocol').should('contains', 'http')
+        amazonTest.elements.navLogo().should('exist')
+    })
+
+    it.only('should search for alexa', () => {
+        amazonTest.typeSearchBar('alexa')
+        amazonTest.elements.searchBar().type('{enter}')
+        amazonTest.clickNextPage()
+        amazonTest.clickThirdElement()
+        amazonTest.elements.addToCart().should('exist')
+        cy.wait(3000)
+        amazonTest.clickAddToCart()
     })
     
       
--- cypress/pages/amazonTest.js
+++ cypress/pages/amazonTest.js
@@ -4,8 +4,8 @@ class amazonTest {
         navLogo: () => cy.get('#nav-logo-sprites'),
         searchBar: () => cy.get('#twotabsearchtextbox'),
         nextPage: () => cy.get('.s-pagination-next'),
-        thirdElement: () => cy.xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[4]/div/div/div'),
-        addToCart: () => cy.xpath('/html/body/div[1]/div[2]/div[9]/div[6]/div[1]/div[4]/div/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div/form/div/div/div[13]/div[1]/span/span/span/input'),
+        thirdElement: () => cy.xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[6]/div/div/div/div/div/div[1]/div/div[2]/div/span/a/div/img'),
+        addToCart: () => cy.xpath('/html/body/div[1]/div[2]/div[9]/div[6]/div[1]/div[5]/div/div[1]/div/div/div/form/div/div/div/div/div[3]/div/div[13]/div[1]/span/span'),
         addedAlert: () => cy.get('#attachDisplayAddBaseAlert > .a-box-inner > .a-alert-heading')
     }
 
@@ -19,7 +19,7 @@ class amazonTest {
         this.elements.thirdElement().click()
     }
     clickAddToCart() {
-        this.elements.thirdElement().click()
+        this.elements.addToCart().click()
     }
 
 }
