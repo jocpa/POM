3f32c369dc734d9b5843888d51a0d15435347961
Pablo Arzu <jptomas38@gmail.com>
2023-04-01 16:46:04 -0600

    Creating new test

---
+++ cypress/e2e/datePicker.cy.js
@@ -0,0 +1,4 @@
+describe('Date picker Test', () => {
+    const url  = 'https://react-select.com/home'
+    cy.visit(url)
+})
---
+++ cypress/e2e/practiceForm.cy.js
@@ -0,0 +1,4 @@
+describe('Practice form Test', () => {
+    const url = 'https://itera-qa.azurewebsites.net/home/automation'
+    cy.visit(url)
+})
