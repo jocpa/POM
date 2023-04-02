807b92c2166344fd4270e3829b444911911e0f38
Pablo Arzu <jptomas38@gmail.com>
2023-04-02 14:04:13 -0600

    datePicker.js: elements and methods created

--- cypress/pages/datePicker.js
+++ cypress/pages/datePicker.js
@@ -2,4 +2,19 @@
 
 class datePicker {
     
+    elements= {
+        h1Text: () => cy.get('h1'),
+        inputDate: () => cy.get('datepicker-overview-example'),
+        inputIcon: () => cy.get('datepicker-overview-example.ng-star-inserted > .mat-mdc-form-field > .mat-mdc-text-field-wrapper > .mat-mdc-form-field-flex > .mat-mdc-form-field-icon-suffix > .mat-datepicker-toggle > .mdc-icon-button > .mat-mdc-button-touch-target').click()
+    }
+    
+    typeDate(date) {
+        this.elements.inputDate().type(date)
+    }
+    clickIcon() {
+        this.elements.inputIcon().click()
+    }
+
 }
+
+module.exports = new datePicker();
