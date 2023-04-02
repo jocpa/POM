eee4937df88769ad4e182fa2221c2306444fb701
Pablo Arzu <jptomas38@gmail.com>
2023-04-02 14:38:23 -0600

    practiceForm.cy.js: test aproved

--- cypress/e2e/practiceForm.cy.js
+++ cypress/e2e/practiceForm.cy.js
@@ -1,7 +1,26 @@
+import practiceForm from "../pages/practiceForm"
+
+///<reference types="cypress" />
+
 describe('Practice form Test', () => {
     const url = 'https://itera-qa.azurewebsites.net/home/automation'
     
-    it('should extract a date from the datepicker', () => {
+    it('should fill the fields of the form', () => {
         cy.visit(url)
+
+        practiceForm.elements.logo().should('exist')
+        practiceForm.elements.headerText().invoke('text')
+                                          .should('eq', 'Test automation practice form')
+        
+        practiceForm.typeName('Miles')
+        practiceForm.typeMobileNumber('54168148')
+        practiceForm.typeEmail('Miles@miles.com')
+        practiceForm.typePassword('password123')
+        practiceForm.typeAddres('brooklyn')
+        practiceForm.gender()
+        practiceForm.bestDay()
+        practiceForm.travelCountry(1)
+        practiceForm.experience()
+        practiceForm.framework()
     })
 })
